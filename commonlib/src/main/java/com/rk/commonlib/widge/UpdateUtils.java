be3924927c431bd9f0a54996551088787c534a44
gaofengx <xugaofeng@topscomm.com>
2020-06-22 10:13:25 +0800

    add update utils

---
+++ commonlib/src/main/java/com/rk/commonlib/widge/UpdateUtils.java
@@ -0,0 +1,44 @@
+package com.rk.commonlib.widge;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+
+import com.rk.commonlib.UpdateChecker;
+
+import static com.rk.commonlib.download.AppVersion.NEW_VERSION;
+
+public class UpdateUtils {
+    private static final String TAG = UpdateUtils.class.getSimpleName();
+    private static UpdateChecker mUpdateChecker;
+
+    public static void checkUpdate(Context context, String updateUrl) {
+
+        Handler handler = new Handler(Looper.getMainLooper()){
+            @Override
+            public void handleMessage(Message msg) {
+                int what = msg.what;
+                Log.i(TAG, "checkUpdate, handleMessage, what: " + what);
+                switch (what){
+                    case NEW_VERSION://url
+                        if (mUpdateChecker != null) {
+                            mUpdateChecker.showUpdateDialog();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                super.handleMessage(msg);
+            }
+        };
+        UpdateChecker.apkFileName = "ChargeNewVersion.apk";
+        mUpdateChecker = new UpdateChecker(context, handler);
+        String checkUrl = updateUrl;
+        mUpdateChecker.setCheckUrl(checkUrl);
+        mUpdateChecker.setShowAlert(true);
+        mUpdateChecker.setCheckMessage("已是最新");
+        mUpdateChecker.checkUpdates();
+    }
+}
