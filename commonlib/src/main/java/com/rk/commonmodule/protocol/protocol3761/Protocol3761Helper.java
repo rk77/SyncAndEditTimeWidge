2531375c94445442af48f951917dcb09d2314b41
gaofengx <xugaofeng@topscomm.com>
2021-04-24 09:57:36 +0800

    get local module version info in 376.1

--- commonlib/src/main/java/com/rk/commonmodule/protocol/protocol3761/Protocol3761Helper.java
+++ commonlib/src/main/java/com/rk/commonmodule/protocol/protocol3761/Protocol3761Helper.java
@@ -42,7 +42,7 @@ public class Protocol3761Helper {
     public static byte[] makeGetRemoteCommuModuleVersionInfoFrame() {
         Protocol3761Frame.AddrArea addrArea = new Protocol3761Frame.AddrArea("FFFF",0xFFFF, 0x14);
         Protocol3761Frame.DA da = new Protocol3761Frame.DA(0);
-        Protocol3761Frame.DT dt = new Protocol3761Frame.DT(0);
+        Protocol3761Frame.DT dt = new Protocol3761Frame.DT(9);
         Protocol3761Frame.DataUnitID dataUnitID = new Protocol3761Frame.DataUnitID(da, dt);
         byte[] userLinkData = Protocol3761.PROTOCOL_3761.makeLinkUserData((byte) 0x09, (byte) 0x60, dataUnitID, null);
         if (userLinkData == null || userLinkData.length <= 0) {
@@ -84,6 +84,50 @@ public class Protocol3761Helper {
                 DataConvertUtils.getSubByteArray(data_unit, 23, 25));
         values[6] = DataConvertUtils.getByteArray2AsciiString(
                 DataConvertUtils.getSubByteArray(data_unit, 26, 45));
+
+        return values;
+    }
+
+    public static byte[] makeGetLocalCommuModuleVersionInfoFrame() {
+        Protocol3761Frame.AddrArea addrArea = new Protocol3761Frame.AddrArea("FFFF",0xFFFF, 0x14);
+        Protocol3761Frame.DA da = new Protocol3761Frame.DA(0);
+        Protocol3761Frame.DT dt = new Protocol3761Frame.DT(10);
+        Protocol3761Frame.DataUnitID dataUnitID = new Protocol3761Frame.DataUnitID(da, dt);
+        byte[] userLinkData = Protocol3761.PROTOCOL_3761.makeLinkUserData((byte) 0x09, (byte) 0x60, dataUnitID, null);
+        if (userLinkData == null || userLinkData.length <= 0) {
+            LogUtils.i("userLinkData is null");
+            return null;
+        }
+        byte[] userDataArea = Protocol3761.PROTOCOL_3761.makeUserDataArea((byte) 0x4A, addrArea, userLinkData);
+        if (userDataArea == null || userDataArea.length <= 0) {
+            LogUtils.i("userDataArea is null");
+            return null;
+        }
+        byte[] frame = Protocol3761.PROTOCOL_3761.makeFrame(userDataArea);
+        return frame;
+    }
+
+    public static String[] parseLocalModuleVersionInfoFrame(byte[] frame) {
+        boolean isOk = Protocol3761.PROTOCOL_3761.verifyFrame(frame);
+        LogUtils.i("isOk: " + isOk);
+        if (!isOk) {
+            return null;
+        }
+
+        byte[] data_unit = DataConvertUtils.getSubByteArray(frame, Protocol3761.PROTOCOL_3761.mSEQPos + 1 + 4, Protocol3761.PROTOCOL_3761.mCsPos - 1);
+        if (data_unit == null || data_unit.length < 15) {
+            return null;
+        }
+        String[] values = new String[5];
+        values[0] = DataConvertUtils.convertByteArrayToString(data_unit, 0, 5, false);
+        values[1] = DataConvertUtils.getByteArray2AsciiString(
+                DataConvertUtils.getSubByteArray(data_unit, 6, 7));
+        values[2] = DataConvertUtils.getByteArray2AsciiString(
+                DataConvertUtils.getSubByteArray(data_unit, 8, 9));
+        values[3] = DataConvertUtils.convertByteArrayToString(data_unit, 12, 12, false) + "-"
+                + DataConvertUtils.convertByteArrayToString(data_unit, 111, 11, false) + "-"
+                + DataConvertUtils.convertByteArrayToString(data_unit, 10, 10, false);
+        values[4] = DataConvertUtils.convertByteArrayToString(data_unit, 13, 14, false);
 
         return values;
     }
