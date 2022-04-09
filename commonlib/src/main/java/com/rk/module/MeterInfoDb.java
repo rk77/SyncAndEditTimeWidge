80758a7f3eb5bcaf7dc96208df7e3c53ce33c07d
gaofengx <xugaofeng@topscomm.com>
2022-04-09 16:26:13 +0800

    add meter info

--- commonlib/src/main/java/com/rk/module/MeterInfoDb.java
+++ commonlib/src/main/java/com/rk/module/MeterInfoDb.java
@@ -15,4 +15,100 @@ public class MeterInfoDb extends LitePalSupport {
     private String port;
     private String phase;
     private int terminalId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getPt() {
+        return pt;
+    }
+
+    public void setPt(String pt) {
+        this.pt = pt;
+    }
+
+    public String getCt() {
+        return ct;
+    }
+
+    public void setCt(String ct) {
+        this.ct = ct;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getProto() {
+        return proto;
+    }
+
+    public void setProto(String proto) {
+        this.proto = proto;
+    }
+
+    public String getBaudrate() {
+        return baudrate;
+    }
+
+    public void setBaudrate(String baudrate) {
+        this.baudrate = baudrate;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getPhase() {
+        return phase;
+    }
+
+    public void setPhase(String phase) {
+        this.phase = phase;
+    }
+
+    public int getTerminalId() {
+        return terminalId;
+    }
+
+    public void setTerminalId(int terminalId) {
+        this.terminalId = terminalId;
+    }
 }
