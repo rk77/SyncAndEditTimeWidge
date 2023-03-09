0fe5a8de63838f75c9da4c7d2e16dc08e5b6ef74
gaofengx <xugaofeng@topscomm.com>
2023-03-09 14:08:40 +0800

    add file read

---
+++ commonlib/src/main/java/com/rk/commonlib/files/FileReader.java
@@ -0,0 +1,31 @@
+package com.rk.commonlib.files;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public class FileReader {
+    public static String readToString(String fileName) {
+        String encoding = "UTF-8";
+        File file = new File(fileName);
+        Long fileLength = file.length();
+        byte[] fileContent = new byte[fileLength.intValue()];
+        try {
+            FileInputStream in = new FileInputStream(file);
+            in.read(fileContent);
+            in.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            return new String(fileContent, encoding);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
